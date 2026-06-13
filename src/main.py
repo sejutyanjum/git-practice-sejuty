62416863da6a0fa077dac019e3ed933978daf2d2
Sejuty Anjum <sejutyanjum21@gmail.com>
2026-06-13 23:12:12 +0600

    Add multiplication function

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
-from utils import add, subtract
+from utils import add, subtract, multiply
 
 print("Name: Sejuty Anjum")
 print("Today's Date: 13/6/26")
 
 print("Addition:", add(10, 5))
 print("Subtraction:", subtract(10, 5))
+print("Multiplication:", multiply(10, 5))
